f14b3f3265cfd6990ee708fb496af7784c13be06
bbarbis <baratebarbara2020@gmail.com>
2023-02-01 16:55:06 +0100

    osszegzes

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 # megoldas
+def pontszamitas():
+    ossz = 0
+    for i in range (len(lista)):
+        ossz += lista[i]
+            return ossz
 
-def osszegzes():
+def eredmeny(jatekos, gep):
     if jatekos_P > 21:
         print("Játekos veszít")
     elif gep_P > 21:
         print("Gép veszít")
+
 
 
 # esetek
