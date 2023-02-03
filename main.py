b685f57689551e7d8b93fb0a83a19fce9512530b
bbarbis <baratebarbara2020@gmail.com>
2023-02-03 12:25:19 +0100

    tesztesetek

--- main.py
+++ main.py
@@ -1,17 +1,38 @@
 # megoldas
-def pontszamitas(lapok :):
-    lap = []
-    osszead = 0
-    for i in range (len(lista)):
-        osszead += lapok[i]
+def pontszamitas(lapok):
 
+    osszead = 0
+    for i in range(len(lapok)):
+        osszead += lapok[i]
+        return osszead
 
 def eredmeny(jatekos_L, gep_L):
-    if jatekos_P > 21:
-        print("Játekos veszít")
-    elif gep_P > 21:
-        print("Gép veszít")
+    szoveg = ""
+    jatekos = pontszamitas(jatekos_L)
+    gep = pontszamitas(gep_L)
+
+    if jatekos > 21:
+        szoveg = "Játekos veszít"
+    elif gep > 21:
+        szoveg = "Gép veszít"
 
 
 
 # esetek
+def jatekosVesztettTeszt():
+    jatekosLista = [6, 4, 8, 9]
+    gepLista = [6, 4, 11]
+
+    kapottEredmeny = eredmeny(jatekosLista, gepLista)
+    vartEredmeny = "Jatekos veszített"
+
+    if kapottEredmeny == vartEredmeny:
+        print("Teszt sikeres. :)")
+    else:
+        print("Teszt megbukott. :)")
+
+
+def tesztek():
+    jatekosVesztettTeszt()
+
+tesztek()
